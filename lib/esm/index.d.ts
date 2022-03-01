53206104fa5458fde7b96b72949706b709984e69
ocean-rey <x7flux@gmail.com>
2022-03-01 17:31:38 +0300

    new binaries

--- lib/esm/index.d.ts
+++ lib/esm/index.d.ts
@@ -3,54 +3,66 @@ export default class ZoomClient {
     #private;
     _zoom: AxiosInstance;
     constructor({ apiKey, secretKey, timezone, user }: ZoomClientParams);
-    createSingleWebinar({ start, end, name, agenda, account, password, approval, recording, }: CreateSingleWebinarParams): Promise<string>;
-    createRecurringWebinar({ ...options }: createRecurringWebinarParams): Promise<unknown>;
+    createSingleWebinar({ start, duration, name, agenda, account, password, approval, recording, }: CreateWebinarBaseParams): Promise<string>;
+    createRecurringWebinar({ ...options }: CreateRecurringWebinarParams): Promise<unknown>;
     registerToWebinar({ webinarID, firstName, lastName, email, }: RegisterToWebinarParams): Promise<string>;
     getWebinarAttendees(webinarID: string): Promise<Participation[]>;
 }
-export declare type createRecurringWebinarParams = {
-    start: Date;
-    endAfter: Date | number;
-    name: string;
-    duration: number;
-    approval?: Approval;
-    recording?: Recording;
-    agenda?: string;
-    account?: string;
+declare type Recording = "local" | "cloud" | "none";
+declare type Approval = "registration" | "registration+approval" | "none";
+declare type Recurrence = "daily" | "weekly" | "monthly";
+declare type DayOfWeek = "sunday" | "monday" | "tuesday" | "wednesday" | "thursday" | "friday" | "saturday";
+declare type RecurrenceParams = {
     type: Recurrence;
     interval: number;
-    monthlyWeek: -1 | 1 | 2 | 3 | 4;
-    weekdays?: DayOfWeek[];
-    monthlyDay: number;
-    password: string;
+    endAfter: Date | Number;
 };
-export declare type Recurrence = "daily" | "weekly" | "monthly";
-export declare type DayOfWeek = "sunday" | "monday" | "tuesday" | "wednesday" | "thursday" | "friday" | "saturday";
-export declare type ZoomClientParams = {
+declare type WeeklyRecurrence = RecurrenceParams & {
+    type: "weekly";
+    params: {
+        weekdays: DayOfWeek[];
+    };
+};
+declare type MonthlyRecurrence = RecurrenceParams & {
+    type: "monthly";
+    params: {
+        day: number;
+    } | {
+        week: -1 | 1 | 2 | 3 | 4;
+        weekdays: DayOfWeek[];
+    };
+};
+declare type DailyRecurrence = RecurrenceParams & {
+    type: "daily";
+};
+declare type RecurrenceOptions = WeeklyRecurrence | MonthlyRecurrence | DailyRecurrence;
+declare type CreateRecurringWebinarParams = CreateWebinarBaseParams & RecurrenceOptions & {
+    endAfter: Date | number;
+    interval: number;
+};
+declare type ZoomClientParams = {
     apiKey: string;
     secretKey: string;
     timezone: string;
     user: string;
 };
-export declare type Recording = "local" | "cloud" | "none";
-export declare type Approval = "registration" | "registration+approval" | "none";
-export declare type CreateSingleWebinarParams = {
+declare type CreateWebinarBaseParams = {
     start: Date;
-    end: Date;
     name: string;
     agenda?: string;
     account?: string;
     password?: string;
     approval?: Approval;
     recording?: Recording;
+    duration: number;
 };
-export declare type RegisterToWebinarParams = {
+declare type RegisterToWebinarParams = {
     webinarID: string;
     firstName: string;
     lastName: string;
     email: string;
 };
-export declare type Participation = {
+declare type Participation = {
     id: string;
     user_id: string;
     name: string;
@@ -59,3 +71,4 @@ export declare type Participation = {
     leave_time: Date | string;
     duration: number;
 };
+export {};
